f417ce8b6a049a076982374a44b86551538e7ab1
Jan Lipnican <jan.lipnican@cognito.cz>
2016-11-03 18:59:15 +0100

    Deleted unused things

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
 import React from 'react';
-import App from 'App.jsx';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router, Route, Link, browserHistory } from 'react-router';
+
+import Store from 'Store';
+
+// Pages
+import Homepage from 'Routes/Homepage/Controller.jsx';
+import About from 'Routes/About/Controller.jsx';
+
+class App extends React.Component {
+    render() {
+        return (
+            <Provider store={Store}>
+                <Router history={browserHistory}>
+                    <Route path="/" component={Homepage}/>
+                    <Route path="/about" component={About}/>
+                </Router>
+            </Provider>
+        );
+    }
+}
 
 // initialise app
 document.addEventListener("DOMContentLoaded", function(event) { 
